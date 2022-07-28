317eab902c5622e0c698b8471f99ce7744258477
xjq <xiajq01@mingyuanyun.com>
2022-07-28 14:16:01 +0800

    update

--- src/data.ts
+++ src/data.ts
@@ -478,6 +478,11 @@ const content: Source[] = [
         link: 'https://github.com/kamranahmedse/developer-roadmap',
         desc: 'Roadmap to becoming a developer in 2022',
       },
+      {
+        title: 'GitHub中文排行榜',
+        link: 'https://github.com/GrowingGit/GitHub-Chinese-Top-Charts',
+        desc: '帮助你发现优秀中文项目，可以无语言障碍地、更高效地吸收优秀经验成果',
+      },
     ],
   },
 ];
